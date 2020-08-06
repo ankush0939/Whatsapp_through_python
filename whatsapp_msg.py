cb5310d5b2b7d603d9a617972436ecaa5d3b3dab
Ankush Mishra <56724751+ankush0939@users.noreply.github.com>
2020-08-06 14:28:54 +0530

    Update whatsapp_msg.py

--- whatsapp_msg.py
+++ whatsapp_msg.py
@@ -1,3 +1,9 @@
-import pywhatkit
+import pywhatkit #first one has to install the pywhatkit library and then import it
 
-pywhatkit.sendwhatmsg("+919438746476"," Testing Msg via python",14,10)
+pywhatkit.sendwhatmsg("number","message",h,m)
+#sendwhatmsg is a function defined in pywhatkit library which takes four parameters
+#the first parameter is the number of the reciever enclosed in " ". the number should have +91 extension
+#the next parameter is the message enclosed in " ".
+#the third parameter is the hour of message or the hour component of time of the message to be sent. This should be in 24h format
+#the last parameter is the minute component.
+#follow the steps given in the readme file to accomplish the task.
